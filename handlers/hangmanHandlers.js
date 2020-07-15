dde1845baa53e093dc13136b50d7e6c380b039b5
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-07-15 13:58:30 -0400

    cleaning

--- handlers/hangmanHandlers.js
+++ handlers/hangmanHandlers.js
@@ -12,13 +12,18 @@ const handlerWord = (req, res) => {
 const handlerLetter = (req, res) => {
     const {id, letter} = req.params;
     const {word, boolArr} = func.getWordObj(words, id);
+    let message = 'Try again!';
 
     if (!boolArr) 
         res.status(404).json('oups! wrong word id.. Request /word first!');
 
     for (const c in word)
-        if (word.charAt(c) === letter) boolArr[c] = ` ${letter} `;
-    res.status(200).json(JSON.stringify(boolArr).replace(/"/g, ""));
+        if (word.charAt(c) === letter) {
+            boolArr[c] = ` ${letter} `;
+            message = 'Yesss!'
+        }
+
+    res.status(200).json(JSON.stringify(boolArr).replace(/"/g, "") + ' ' + message);
 };
 
 module.exports = { handlerWord, handlerLetter };
