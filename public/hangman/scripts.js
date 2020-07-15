34dbc6e5a4339be24d0a21b81691a7a66866e44c
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-07-15 16:51:04 -0400

    added game ending

--- public/hangman/scripts.js
+++ public/hangman/scripts.js
@@ -7,16 +7,36 @@ const inputDiv = document.createElement('div');
 const inputLetter = document.createElement('input');
 const checkBtn = document.createElement('button');
 const resultDiv = document.createElement('div');
+const againBtn = document.createElement('button');
 
 let wordId;
 
 //functions
 
+const endGame = (str) => {
+    checkBtn.remove();
+    inputDiv.remove();
+    const newStr = str.replace(/,/g, '');
+    resultDiv.innerText = newStr.replace(/Yesss!/, 'You win!');
+    againBtn.innerText = 'AGAIN!';
+    wordWrapper.appendChild(againBtn);
+    againBtn.addEventListener('click', () => location.reload());
+}
+
+//returns true if is a letter
 const isLetter = (char) => {
     const objRegExp = /^[a-z]+$/;
     return objRegExp.test(char);
 };
 
+//returns true if word complete
+const isWordComplete = (str) => {
+    if (str.includes('_'))
+        return false;
+    return true;
+};
+
+// creates input and appends divs for game interaction
 const createInput = () => {
     startButton.style.display = 'none';
     wordWrapper.appendChild(resultDiv);
@@ -28,6 +48,7 @@ const createInput = () => {
     wordWrapper.appendChild(checkBtn);
 };
 
+//start function
 const newWord = async () => {
     try {
         const res = await fetch('/hangman/word');
@@ -40,18 +61,20 @@ const newWord = async () => {
     }
 };
 
+// every time the user guesses a letter
 const checkLetter = async (letter) => {
     try {
         if (isLetter(letter)) {
             const res = await fetch(`/hangman/guess/${wordId}/${letter}`);
             const ans = await res.json();
-            resultDiv.innerText = ans;    
+            (isWordComplete(ans)) ? endGame(ans) : resultDiv.innerText = ans; 
         } else
             resultDiv.innerText = "Enter a letter!" ;
     } catch (err) {
         console.log(err);
     }
 }
+
 
 // start button function, getting new word
 startButton.addEventListener('click', newWord);
